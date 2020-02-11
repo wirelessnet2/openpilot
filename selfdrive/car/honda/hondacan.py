229c27811793975a74d11e41338548e8c936245e
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-02-11 01:39:29 -0500

    Revert "Remove some unnecessary changes from stock"
    This reverts commit 2324fcaceef1a5218cd60f18d4c0f16a8f92b5b3.

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -60,7 +60,7 @@ def create_brake_command(packer, apply_brake, pcm_override, pcm_cancel_cmd, fcw,
     "COMPUTER_BRAKE_REQUEST": brake_rq,
     "SET_ME_1": 1,
     "BRAKE_LIGHTS": brakelights,
-    "CHIME": stock_brake["CHIME"] if fcw else 0,  # send the chime for stock fcw
+    "CHIME": 0,  # chime issued when disabling FCM - Clarity
     "FCW": fcw << 1,  # TODO: Why are there two bits for fcw?
     "AEB_REQ_1": 0,
     "AEB_REQ_2": 0,
@@ -103,10 +103,10 @@ def create_ui_commands(packer, pcm_speed, hud, car_fingerprint, is_metric, idx, 
       'IMPERIAL_UNIT': int(not is_metric),
       'SET_ME_X01_2': 1,
       'SET_ME_X01': 1,
-      "FCM_OFF": stock_hud["FCM_OFF"],
-      "FCM_OFF_2": stock_hud["FCM_OFF_2"],
-      "FCM_PROBLEM": stock_hud["FCM_PROBLEM"],
-      "ICONS": stock_hud["ICONS"],
+      "FCM_OFF": 0, #CLarity: This call on stock_hud[] -wirelessnet2
+      "FCM_OFF_2": 0, #CLarity: This call on stock_hud[] -wirelessnet2
+      "FCM_PROBLEM": 0, #CLarity: This call on stock_hud[] -wirelessnet2
+      "ICONS": 0, #CLarity: This call on stock_hud[] -wirelessnet2
     }
     commands.append(packer.make_can_msg("ACC_HUD", bus_pt, acc_hud_values, idx))
 
