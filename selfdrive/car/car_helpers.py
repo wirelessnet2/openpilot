de73a3d5b2bf5893eac1b32682b40f9bec090bb9
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-02-11 20:46:52 -0500

    Bring Fingerprinting V2 to Clarity
    This code MASSIVELY speeds up the get_fw_versions() function, greatly reducing the time needed for Fingerprinting V2 to actually fingerprint the car. Previously it would take so long that the car would enter an error sate because while OpenPilot is performing the fingerprint, it will not send any messages over CAN. This allows for better fingerprinting and also for eps_modified detection for FW modded EPS's. I also brought all of these features to White and Grey Panda users. OBD-II-less installs are now supported as well. When using White Panda, Grey Panda, or OBD-II-less, the VIN fingerprinting capability will be unavailable.

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -62,25 +62,27 @@ def only_toyota_left(candidate_cars):
 
 
 # **** for use live only ****
-def fingerprint(logcan, sendcan, has_relay):
+def fingerprint(logcan, sendcan, has_relay): #I reworked a good portion of fingerprint code to MASSIVELY SPEED UP GET_FW_VERSIONS() and bring fingerprinting v2 and eps_modified detection to non-relay pandas and comma power-less relay panda installs. -wirelessnet2
   if has_relay:
     # Vin query only reliably works thorugh OBDII
     bus = 1
+    _, vin = get_vin(logcan, sendcan, bus)
+  else:
+    vin = VIN_UNKNOWN
 
-    """cached_params = Params().get("CarParamsCache")
-    if cached_params is not None:
-      cloudlog.warning("Using cached CarParams")
-      CP = car.CarParams.from_bytes(cached_params)
-      vin = CP.carVin
-      car_fw = list(CP.carFw)
-    else:
-      _, vin = get_vin(logcan, sendcan, bus)
-      car_fw = get_fw_versions(logcan, sendcan, bus)
-
-    fw_candidates = match_fw_to_car(car_fw)
-  else:"""  #Clarity: This makes the Black Panda/Uno take forever to begin sending messages upon Ignition on. -wirelessnet2
-  vin = VIN_UNKNOWN
-  fw_candidates, car_fw = set(), []
+  bus = 2
+  car_fw = get_fw_versions(logcan, sendcan, bus)
+  fw_candidates = match_fw_to_car(car_fw)
+  
+  if car_fw is None:
+    fw_candidates, car_fw = set(), []
+    
+  bus = 1
+  
+  print("printing VIN")
+  print(vin)
+  print("printing fw versions")
+  print(car_fw)
 
   cloudlog.warning("VIN %s", vin)
   Params().put("CarVin", vin)
