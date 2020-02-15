604ec9eb205744f4484a93127089987a6c95325b
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-02-15 18:40:03 -0500

    Make LKAS Fault fix better
    Actually fixes the possible runaway HzCounter value

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -512,13 +512,17 @@ class CarInterface(CarInterfaceBase):
 
     # events
     events = []
-    if self.CS.steer_error: #Clarity: This will allow for steer_error to be true for 3 seconds before displaying a warning. -wirelessnet2
-      self.HzCounter += 1
+    if not self.CS.lkMode:
+      events.append(create_event('manualSteeringRequired', [ET.WARNING]))
+    elif self.CS.steer_error: #Clarity: This will allow for steer_error to be true for 3 seconds before displaying a warning. -wirelessnet2
+      while self.HzCounter < 1000:
+        self.HzCounter += 1
+        break
       if self.HzCounter > 300:
         events.append(create_event('steerUnavailable', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE, ET.PERMANENT]))
-    elif self.CS.steer_warning:
+    elif self.CS.steer_warning and self.CS.lkMode:
       events.append(create_event('steerTempUnavailable', [ET.WARNING]))
-    elif not self.CS.steer_error:
+    if not self.CS.steer_error:
       self.HzCounter = 0
     if self.CS.brake_error:
       events.append(create_event('brakeUnavailable', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE, ET.PERMANENT]))
