9c6ded7f7bc58a292933ae99a27cba933a919ec2
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-02-20 16:24:27 -0500

    Update measure_steering_accuracy.py

--- selfdrive/debug/measure_steering_accuracy.py
+++ selfdrive/debug/measure_steering_accuracy.py
@@ -2,7 +2,8 @@
 import os
 import argparse
 import signal
-from collections import defaultdict
+from collections import deque, defaultdict
+from statistics import mean
 
 import cereal.messaging as messaging
 
@@ -24,59 +25,56 @@ if __name__ == "__main__":
   carControl = messaging.sub_sock('carControl', addr=args.addr, conflate=True)
   sm = messaging.SubMaster(['carState', 'carControl', 'controlsState'], addr=args.addr)
 
-  miss_cnt = 0
-  speed = [0] * 10
-  angle_actual = [0] * 10
-  angle_desire = [0] * 10
-  error = [0] * 10
+  msg_cnt = 0
   stats = defaultdict(lambda: {'err': 0, "cnt": 0, "=": 0, "+": 0, "-": 0})
   cnt = 0
   total_error = 0
 
   while messaging.recv_one(carControl):
     sm.update()
-    miss_cnt += 1
+    msg_cnt += 1
 
     actual_speed = sm['carState'].vEgo
     enabled = sm['controlsState'].enabled
     steer_override = sm['controlsState'].steerOverride
-    actual_angle = sm['controlsState'].angleSteers
-    desired_angle = sm['carControl'].actuators.steerAngle
-    angle_error = desired_angle - actual_angle
 
+    # must be above 10 m/s, engaged and not overriding steering
     if actual_speed > 10.0 and enabled and not steer_override:
-      speed.append(actual_speed)
-      speed.pop(0)
-      angle_actual.append(actual_angle)
-      angle_actual.pop(0)
-      angle_desire.append(desired_angle)
-      angle_desire.pop(0)
-      error.append(angle_error)
-      error.pop(0)
       cnt += 1
 
-      if cnt >= 200 and cnt % 10 == 0:
-        speed_avg = int(round(sum(speed) / len(speed), 0))
-        actual_avg = int(round(sum(angle_actual) / len(angle_actual), 0))
-        desire_avg = int(round(sum(angle_desire) / len(angle_desire), 0))
-        error_avg = abs(round(sum(error) / len(error), 2))
+      # wait 5 seconds after engage/override
+      if cnt >= 500:
+        # calculate error before rounding
+        actual_angle = sm['controlsState'].angleSteers
+        desired_angle = sm['carControl'].actuators.steerAngle
+        angle_error = abs(desired_angle - actual_angle)
 
-        angle_abs = abs(actual_avg)
-        stats[angle_abs]["err"] += error_avg
+        # round numbers
+        actual_angle = round(actual_angle, 1)
+        desired_angle = round(desired_angle, 1)
+        angle_error = round(angle_error, 2)
+        angle_abs = int(abs(round(desired_angle, 0)))
+
+        # collect stats
+        stats[angle_abs]["err"] += angle_error
         stats[angle_abs]["cnt"] += 1
-        if actual_avg == desire_avg:
+        if actual_angle == desired_angle:
           stats[angle_abs]["="] += 1
         else:
-          stats[angle_abs]["+" if actual_avg > desire_avg else "-"] += 1
-      if cnt > 0 and miss_cnt % 100 == 0:
-        print(chr(27) + "[2J")
-        for k in sorted(stats.keys()):
-          v = stats[k]
-          print(f'angle: {k:#2} | error: {round(v["err"] / v["cnt"], 2):2.2f} | =:{int(v["="] / v["cnt"] * 100):#3}% | +:{int(v["+"] / v["cnt"] * 100):#3}% | -:{int(v["-"] / v["cnt"] * 100):#3}% | count: {v["cnt"]:#4}')
-
+          if desired_angle == 0.:
+            overshoot = True
+          else:
+            overshoot = desired_angle < actual_angle if desired_angle > 0. else desired_angle > actual_angle
+          stats[angle_abs]["+" if overshoot else "-"] += 1
     else:
-      speed = [0] * 10
-      angle_actual = [0] * 10
-      angle_desire = [0] * 10
-      error = [0] * 10
       cnt = 0
+
+    if msg_cnt % 100 == 0:
+      print(chr(27) + "[2J")
+      if cnt != 0:
+        print("COLLECTING ...")
+      else:
+        print("DISABLED (speed too low, not engaged, or steer override)")
+      for k in sorted(stats.keys()):
+        v = stats[k]
+        print(f'angle: {k:#2} | error: {round(v["err"] / v["cnt"], 2):2.2f} | =:{int(v["="] / v["cnt"] * 100):#3}% | +:{int(v["+"] / v["cnt"] * 100):#4}% | -:{int(v["-"] / v["cnt"] * 100):#3}% | count: {v["cnt"]:#4}')
