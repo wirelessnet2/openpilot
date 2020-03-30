e874ce6cf0605786db61ef3aaaa651ff8e44da54
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-03-30 13:19:50 -0400

    Enhancements Addon
    This adds:
    -Increased Acceleration when not following a lead car
    -Better Brake Tuning for Clarity

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -13,7 +13,7 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 def actuator_hystereses(brake, braking, brake_steady, v_ego, car_fingerprint):
   # hyst params
   brake_hyst_on = 0.02     # to activate brakes exceed this value
-  brake_hyst_off = 0.005                     # to deactivate brakes below this value
+  brake_hyst_off = 0.001                     # to deactivate brakes below this value
   brake_hyst_gap = 0.01                      # don't change brake command for small oscillations within this value
 
   #*** hysteresis logic to avoid brake blinking. go above 0.1 to trigger
