5c401a9cc2078da20dcd1bddd8e46f681e6ee8c6
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-04-25 13:34:45 -0400

    CPU usage percentage

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -927,7 +927,7 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
     char uom_str[6];
     NVGcolor val_color = nvgRGBA(255, 255, 255, 200);
     snprintf(val_str, sizeof(val_str), "%.0f°C", (scene->cpu0Temp));
-    snprintf(uom_str, sizeof(uom_str), "%d", (s->scene.cpuPerc));
+    snprintf(uom_str, sizeof(uom_str), "%d%%", (s->scene.cpuPerc));
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "CPU TEMP",
         bb_rx, bb_ry, bb_uom_dx,
         val_color, lab_color, uom_color,
