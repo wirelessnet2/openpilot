b8045cc88efc77130cb7224a2a5028d437586ec5
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-05-16 03:41:14 -0400

    Update can_bitbang.h

--- panda/board/drivers/can_bitbang.h
+++ panda/board/drivers/can_bitbang.h
@@ -1,39 +1,33 @@
-void set_bitbang_can_rx_pin(void) {
-    if (/*Need to get orientation of OBD-C connector and change pin accordingly*/)
-    {
-        #define OBD_CAN_RX_PIN 12
-    }
-    else
-    {
-        #define OBD_CAN_RX_PIN 13
-    }
-}
-
 void calculate_bitbang(void) {
     //This will be called in main.c at 1Hz
     //Do the message reconstruction here
     //Write the final message to a variable that we can send over to the EON over USB
 };
 
-void EXTI15_10_IRQ_Handler(void) {
+void bitbang_IRQ_Handler(void) {
 //Interrupt Handler Code Here
 //Probably just write to the ring buffer?
 };
 
 
 void can_bitbang_init(void) {
-    REGISTER_INTERRUPT(EXTI15_10_IRQn, EXTI15_10_IRQ_Handler, 600000U, FAULT_INTERRUPT_RATE_BITBANG_CAN);
-    if(OBD_CAN_RX_PIN == 12) {
-        register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PB, /*CALCULATE MASK*/)
+    if(car_harness_status == HARNESS_STATUS_NORMAL) {
+        REGISTER_INTERRUPT(EXTI15_10_IRQn, bitbang_IRQ_Handler, 600000U, FAULT_INTERRUPT_RATE_BITBANG_CAN);
+        register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PB, /*CALCULATE MASK*/);
+        register_set_bits(&(EXTI->IMR), (1U << 12));
+        register_set_bits(&(EXTI->RTSR), (1U << 12));
+        register_set_bits(&(EXTI->FTSR), (1U << 12));
+        NVIC_EnableIRQ(EXTI15_10_IRQn);
     }
-    else {
-        register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI13_PB, /*CALCULATE MASK*/)
+    else if(car_harness_status == HARNESS_STATUS_FLIPPED) {
+        REGISTER_INTERRUPT(EXTI9_5_IRQn, bitbang_IRQ_Handler, 600000U, FAULT_INTERRUPT_RATE_BITBANG_CAN);
+        register_set(&(SYSCFG->EXTICR[1]), SYSCFG_EXTICR2_EXTI5_PB, /*CALCULATE MASK*/)
+        register_set_bits(&(EXTI->IMR), (1U << 5));
+        register_set_bits(&(EXTI->RTSR), (1U << 5));
+        register_set_bits(&(EXTI->FTSR), (1U << 5));
+        NVIC_EnableIRQ(EXTI9_5_IRQn);
     }
-
-    register_set_bits(&(EXTI->IMR), (1U << OBD_CAN_RX_PIN));
-    register_set_bits(&(EXTI->RTSR), (1U << OBD_CAN_RX_PIN));
-    register_set_bits(&(EXTI->FTSR), (1U << OBD_CAN_RX_PIN));
-
-    NVIC_EnableIRQ(EXTI15_10_IRQn);
+    else{puts("Bitbang CAN init failed!\n")
+    }
 };
 
