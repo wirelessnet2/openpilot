ea0d7caacf3864575b8cd2e9b8720f89bc6f3426
kblake9 <53315512+kblake9@users.noreply.github.com>
2020-05-18 22:10:25 -0400

    Update can_bitbang.h

--- panda/board/drivers/can_bitbang.h
+++ panda/board/drivers/can_bitbang.h
@@ -31,4 +31,60 @@ void can_bitbang_init(void) {
         puts("Bitbang CAN init failed!\n")
     }
 };
+/*
+This is the circular buffer.
+Struct is established, then buffer is initialized with preset size, then head and tail
+are intialized.
+*/
+//initial structure
+typedef struct circ_t {
+    uint8_t *bitbangbuffer;
+    bool bufferFull;
+    bool bufferEmpty;
+    uint16_t bufferHead;
+    uint16_t bufferTail; 
+    uint16_t maxBufferSize;
+} circ;
+
+circ circbuffer_init (circ_t *circ, int16_t maxBufferSize) {
+    circ->bufferFull = 0;
+    circ->bufferEmpty = 1;
+    circ->bufferHead = 0;
+    circ->bufferTail = 0;
+    circ->maxBufferSize = maxBufferSize;
+    circ->bitbangbuffer = (circ *) malloc(sizeof(circ)*maxBufferSize);
+}
+void pushBuffer(circ_t *circ,uint8_t *data) {
+    if (bufferFull == 1) {
+        std::cout << ("Buffer is Full\n");
+    }
+    else{
+        circ->bitbangbuffer[circ->bufferHead] = *data;
+        circ->bufferHead++;
+        circ->bufferEmpty = 0;
+        if ((circ->bufferHead-1) == circ->maxBufferSize) {
+            circ->bufferFull = 1;
+        }
+    }
+}
+uint8_t  popBuffer(circ_t *circ) {
+    uint8_t data;
+    if (circ->bufferEmpty) {
+        std::cout << ("Buffer is Empty\n");
+        return 0;
+    }
+    else{
+        data = circ->bitbangbuffer[circ->bufferTail];
+        circ->bufferTail++;
+        circ->bufferFull = 0;
+        if ((circ->bufferHead == circ->bufferTail) && (circ->bufferFull !=1)) 
+            {circ->bufferEmpty = 1;}
+        else if (circ->bufferHead == (circ->bufferTail-1)) {
+            circ->bufferHead = 0;
+            circ->bufferTail = 0;
+        }
+        return data;
+    }
+}
+
 
