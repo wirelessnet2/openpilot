bd2af311e5aa39827c8685b852ce3a58f7661b2d
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-05-22 17:12:25 -0400

    Add Masks

--- panda/board/drivers/can_bitbang.h
+++ panda/board/drivers/can_bitbang.h
@@ -7,25 +7,28 @@ void calculate_bitbang(void) {
 void bitbang_IRQ_Handler(void) {
 //Interrupt Handler Code Here
 //Probably just write to the ring buffer?
+EXTI->PR = (1U << BITBANG_CAN_PIN);
 };
 
 
 void can_bitbang_init(void) {
     if(car_harness_status == HARNESS_STATUS_NORMAL) {
         REGISTER_INTERRUPT(EXTI15_10_IRQn, bitbang_IRQ_Handler, 600000U, FAULT_INTERRUPT_RATE_BITBANG_CAN);
-        register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PB, /*CALCULATE MASK*/);
+        register_set(&(SYSCFG->EXTICR[3]), SYSCFG_EXTICR4_EXTI12_PB, 0xF);
         register_set_bits(&(EXTI->IMR), (1U << 12));
         register_set_bits(&(EXTI->RTSR), (1U << 12));
         register_set_bits(&(EXTI->FTSR), (1U << 12));
         NVIC_EnableIRQ(EXTI15_10_IRQn);
+        #define BITBANG_CAN_PIN 12
     }
     else if(car_harness_status == HARNESS_STATUS_FLIPPED) {
         REGISTER_INTERRUPT(EXTI9_5_IRQn, bitbang_IRQ_Handler, 600000U, FAULT_INTERRUPT_RATE_BITBANG_CAN);
-        register_set(&(SYSCFG->EXTICR[1]), SYSCFG_EXTICR2_EXTI5_PB, /*CALCULATE MASK*/)
+        register_set(&(SYSCFG->EXTICR[1]), SYSCFG_EXTICR2_EXTI5_PB, 0xF0)
         register_set_bits(&(EXTI->IMR), (1U << 5));
         register_set_bits(&(EXTI->RTSR), (1U << 5));
         register_set_bits(&(EXTI->FTSR), (1U << 5));
         NVIC_EnableIRQ(EXTI9_5_IRQn);
+        #define BITBANG_CAN_PIN 5
     }
     else {
         puts("Bitbang CAN init failed!\n")
