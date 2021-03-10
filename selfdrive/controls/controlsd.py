e11709baa010a7f4935af3f993059b3609e5bf2d
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2021-03-10 01:37:36 -0500

    Hotfix

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -234,7 +234,7 @@ class Controls:
       self.events.add(EventName.relayMalfunction)
     if self.sm['longitudinalPlan'].fcw:
       self.events.add(EventName.fcw)
-    if not self.CP.Found and not self.epsAlertDisplayed:
+    if not self.CP.epsFound and not self.epsAlertDisplayed:
       self.events.add(EventName.epsNotFound)
       self.epsAlertDisplayed = True
 
